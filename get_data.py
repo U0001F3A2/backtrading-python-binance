9769aeddd8438a5af60114a79638caeea6d85cdd
Yoongbok Lee <yoongbok.lee@certik.org>
2022-05-26 22:16:15 +0900

    wow

--- get_data.py
+++ get_data.py
@@ -3,15 +3,16 @@ from binance.client import Client
 
 client = Client(config.API_KEY, config.API_SECRET)
 
+pair = config.BTCUSDT
 
-csvfile = open('BTCUSDT-2017-2020-1mth.csv', 'w', newline='')
+csvfile = open(pair + '-2017-2020-1mth.csv', 'w', newline='')
 candlestick_writer = csv.writer(csvfile, delimiter=',')
 
 
-candlesticks = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_1MONTH, "1 Jan, 2017", "25 Dec, 2020")
+candlesticks = client.get_historical_klines(pair, Client.KLINE_INTERVAL_1MONTH, "1 Jan, 2017")
 
 for candlestick in candlesticks:
-    candlestick[0] = candlestick[0] / 1000 # divide timestamp to ignore miliseconds
+    candlestick[0] = int(candlestick[0] / 1000) # divide timestamp to ignore miliseconds
     print(candlestick[0])
     candlestick_writer.writerow(candlestick)
 
