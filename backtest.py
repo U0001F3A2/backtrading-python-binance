50053485b8f442baebc8742b324c135f97c34b51
Yoongbok Lee <yoongbok.lee@certik.org>
2022-05-26 23:28:23 +0900

    generate data :]

--- backtest.py
+++ backtest.py
@@ -1,6 +1,3 @@
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
-
 import datetime  # For datetime objects
 import backtrader as bt # Import the backtrader platfor
 
--- get_data.py
+++ get_data.py
@@ -3,18 +3,19 @@ from binance.client import Client
 
 client = Client(config.API_KEY, config.API_SECRET)
 
-pair = config.BTCUSDT
+pair = config.ETHUSDT
 
-csvfile = open(pair + '-2017-2020-1mth.csv', 'w', newline='')
-candlestick_writer = csv.writer(csvfile, delimiter=',')
-
-
-candlesticks = client.get_historical_klines(pair, Client.KLINE_INTERVAL_1MONTH, "1 Jan, 2017")
-
-for candlestick in candlesticks:
-    candlestick[0] = int(candlestick[0] / 1000) # divide timestamp to ignore miliseconds
-    print(candlestick[0])
-    candlestick_writer.writerow(candlestick)
+for i in range(len(config.interval)):
+    csvfile = open("data/" + pair + '-2017-2022-' + config.interval[i] + '.csv', 'w', newline='')
+    candlestick_writer = csv.writer(csvfile, delimiter=',')
 
 
-csvfile.close()
+    candlesticks = client.get_historical_klines(pair, config.candle_interval[i], "1 Jan, 2017")
+
+    for candlestick in candlesticks:
+        candlestick[0] = int(candlestick[0] / 1000) # divide timestamp to ignore miliseconds
+        print(candlestick[0])
+        candlestick_writer.writerow(candlestick)
+
+
+    csvfile.close()
